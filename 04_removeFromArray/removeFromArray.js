0a7b22c0f2d2454f95e2c0ee8ac4435d02580a24
ancientalientech <ancientalientech@proton.me>
2025-02-24 20:55:20 +0100

    Solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...params) {
+    for (let i = 0; i < params.length; i++) {
+        arr = arr.filter(val => val !== params[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
