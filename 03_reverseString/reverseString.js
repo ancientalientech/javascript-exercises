6d8058e64c4c8ff58e448d9160cea9ff02473859
ancientalientech <ancientalientech@proton.me>
2025-02-24 20:40:21 +0100

    Solve exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let chars = [];
+    for (let i = str.length -1; i >= 0; i--) {
+        chars.push(str[i]);
+    }
 
+    return chars.join('');
 };
 
 // Do not edit below this line
