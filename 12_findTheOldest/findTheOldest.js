0ad75f524b1e7c09ff0615ed26dbc1896e5509ed
ancientalientech <ancientalientech@proton.me>
2025-03-13 22:53:50 +0100

    Solve exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let oldestAge;
+        let personAge;
+        const currentYear = (new Date()).getFullYear();
+        if (!oldest.yearOfDeath) {
+            oldestAge = currentYear - oldest.yearOfBirth;
+        } else {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
 
+        if (!person.yearOfDeath) {
+            personAge = currentYear - person.yearOfBirth;
+        } else {
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (oldestAge > personAge) {
+            return oldest;
+        } else {
+            return person;
+        }
+    })
 };
 
 // Do not edit below this line
