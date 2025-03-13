8c3711cddffc6f835ad10f1fac6cbbee8dc75740
ancientalientech <ancientalientech@proton.me>
2025-03-13 22:07:47 +0100

    Solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    let testWord = "";
 
+    for(let i = 0; i < word.length; i++) {
+        if (('a' <= word[i] && word[i] <= 'z') || ('0' <= word[i] && word[i] <= '9'))
+            testWord += word[i];
+    }
+
+    const halfWayIndex = Math.floor(testWord.length / 2);
+
+    for(let i = 0, j = testWord.length - 1; i < halfWayIndex; i++, j--) {
+        if(testWord[i] !== testWord[j]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+//palindromes('ZZZZ car, a man, a maracaz.');
 
 // Do not edit below this line
 module.exports = palindromes;
